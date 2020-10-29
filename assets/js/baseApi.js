ac718aceb140ebd3a054fe744ce1d150c1dd7061
cheng <89339841@qq.com>
2020-10-29 20:48:57 +0800

    完成首页功能项

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,7 +1,7 @@
 //开发地址
 var local = 'http://ajax.frontend.itheima.net'
 // ................................
-
+//这个方法ajaxPrefilter 只要发送ajax 就会执行
 $.ajaxPrefilter(function(param) {
     // console.log(param.url);
     param.url = local + param.url
@@ -11,6 +11,16 @@ $.ajaxPrefilter(function(param) {
     if(param.url.indexOf('/my/') !==-1) {
         param.headers = {
             Authorization: localStorage.getItem('token')||''
+        }
+    }
+    //complete ：当请求完成时调用的函数  即服务器返回时候
+    //ajaxComplete() 方法规定的函数会在请求完成时运行，即使请求并未成功。
+    param.complete = function (res) {
+        console.log(res.responseJSON);
+        //实现拦截功能,不能通过直接输入网址进入后台
+        if(res.responseJSON.status === 1 && res.responseJSON.message ==='身份认证失败！'){
+            location.href='/login.html'
+            localStorage.removeItem('token')
         }
     }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,23 @@
 $(function () {
     getInfo()
+    var layer = layui.layer
+    $('#loginOut').on('click',function () {
+        //使用layui的插件实现弹出框询问
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //只用点击确定的时候才会指向这里的函数
+            //实现页面跳转,清空token
+            location.href='/login.html'
+            localStorage.removeItem('token')
+            layer.close(index);
+          });
+    })
 })
 
 // 获取用户基恩信息
 function getInfo() {
     $.ajax({
         url:'/my/userinfo',
+        //success ： 当请求成功时调用的函数
         success :function (res) {
             // console.log(res);
             if (res.status !== 0) {
